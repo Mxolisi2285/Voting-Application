4aaf9dbb02d5d81e98faa96f754fdf0a52c1fc90
FacelessObject2285 <andrewmasina2285@gmail.com>
2024-12-17 08:36:13 +0200

    tailwind

---
+++ votingApplication/src/footerComponent.jsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white text-center py-6">
+      <p>&copy; 2024 VoteHub. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
